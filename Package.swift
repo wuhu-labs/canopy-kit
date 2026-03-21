cfb449af478e99dbd8126416e8d56720ebc3957b
Liu, Minsheng <lambda@liu.ms>
2026-03-21 22:34:39 +0800

    Use Heap from swift-collections in refreshResolvedTree instead of array+sort

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     .library(name: "CanopyKit", targets: ["CanopyKit"]),
   ],
   dependencies: [
+    .package(url: "https://github.com/apple/swift-collections", from: "1.1.0"),
     .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "1.0.0"),
     .package(url: "https://github.com/swiftlang/swift-markdown.git", from: "0.7.3"),
   ],
@@ -15,6 +16,7 @@ let package = Package(
     .target(
       name: "CanopyKit",
       dependencies: [
+        .product(name: "Collections", package: "swift-collections"),
         .product(name: "IdentifiedCollections", package: "swift-identified-collections"),
         .product(name: "Markdown", package: "swift-markdown"),
       ],
